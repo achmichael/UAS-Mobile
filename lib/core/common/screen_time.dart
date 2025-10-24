a504650a1b070c40c6c170cb8ab6df0ab8e089c1
achmichael <achmadmichael4@gmail.com>
2025-10-25 00:17:21 +0700

    fix (permission): remove to invoke method openUsageSettings becaause by default AppUsage request permission automatically when permission is not granted

--- lib/core/common/screen_time.dart
+++ lib/core/common/screen_time.dart
@@ -12,17 +12,17 @@ Future<void> requestScreenTimePermission() async {
     print('✅ Permission granted');
   } else {
     print('⚠️ Permission not granted — opening settings...');
-    await openUsageAccessSettings();
+    // await openUsageAccessSettings();
   }
 }
 
-Future<void> openUsageAccessSettings() async {
-  try {
-    await platform.invokeMethod('openUsageAccessSettings');
-  } on PlatformException catch (e) {
-    print('Failed to open settings: ${e.message}');
-  }
-}
+// Future<void> openUsageAccessSettings() async {
+//   try {
+//     // await platform.invokeMethod('openUsageAccessSettings');
+//   } on PlatformException catch (e) {
+//     print('Failed to open settings: ${e.message}');
+//   }
+// }
 
 Future<Duration> getScreenTimeToday() async {
   final ScreenTime screenTime = ScreenTime();
